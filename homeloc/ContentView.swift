bbdcb84c69e89b6667c0f6daa7c2b21dda6893b2
lijing00333 <lijing00333@163.com>
2024-11-02 22:41:07 +0800

    bugfix

--- homeloc/ContentView.swift
+++ homeloc/ContentView.swift
@@ -10,24 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Label("选择经纬度", systemImage: "globe")
         }
         .padding()
-        ScrollView {
-            ShareLink(item: /*@START_MENU_TOKEN@*/URL(string: "https://developer.apple.com/xcode/swiftui")!/*@END_MENU_TOKEN@*/)
-            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
-                Label(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/, systemImage: "42.circle")
+        VStack {
+            List {
+                Text("A List Item")
+                Text("A Second List Item")
+                Text("A Third List Item")
             }
-            .padding(.horizontal)
-            AngularGradient(gradient: /*@START_MENU_TOKEN@*/Gradient(colors: [Color.red, Color.blue])/*@END_MENU_TOKEN@*/, center: /*@START_MENU_TOKEN@*/.zero/*@END_MENU_TOKEN@*/)
-                .padding(.all)
-                .frame(width: /*@START_MENU_TOKEN@*/300.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/103.0/*@END_MENU_TOKEN@*/)
         }
-        .padding(.trailing)
-        
+
     }
 }
 
